b8fad6f1c1f4cd11d39a6b183b8f10c0f777d6cc
Noueman Khalikine <noueman.khalikine@datadoghq.com>
2023-02-06 17:03:59 +0100

    bump versions and remove tile install from prepare script

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -4,9 +4,9 @@ set -e
 
 RESOURCES_DIR="tile/resources"
 
-AGENT_VERSION=${AGENT_VERSION:-4.11.0}
-CLUSTER_AGENT_VERSION=${CLUSTER_AGENT_VERSION:-2.3.0}
-NOZZLE_RELEASE_VERSION=${NOZZLE_RELEASE_VERSION:-85}
+AGENT_VERSION=${AGENT_VERSION:-4.13.1}
+CLUSTER_AGENT_VERSION=${CLUSTER_AGENT_VERSION:-2.5.1}
+NOZZLE_RELEASE_VERSION=${NOZZLE_RELEASE_VERSION:-86}
 
 # download agent bosh release tarball
 curl -L "https://cloudfoundry.datadoghq.com/datadog-agent/datadog-agent-boshrelease-$AGENT_VERSION.tgz" -o $RESOURCES_DIR/datadog-agent-boshrelease.tgz
@@ -16,8 +16,3 @@ curl -L "https://cloudfoundry.datadoghq.com/datadog-cluster-agent/datadog-cluste
 
 # download nozzle bosh release tarball
 curl -L "https://cloudfoundry.datadoghq.com/datadog-firehose-nozzle/datadog-firehose-nozzle-release-$NOZZLE_RELEASE_VERSION.tgz" -o $RESOURCES_DIR/datadog-firehose-nozzle-release.tgz
-
-# install tile-generator
-curl -L "https://github.com/cf-platform-eng/tile-generator/releases/download/v14.0.5/tile_linux-64bit" -o tile_linux-64bit
-chmod +x tile_linux-64bit
-mv tile_linux-64bit /usr/local/bin/tile
