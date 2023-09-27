e31fa53555be41042929a2398a73703efacc8659
Noueman Khalikine <noueman.khalikine@datadoghq.com>
2023-09-27 14:30:19 +0200

    add bpm-release to the tile

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -7,6 +7,7 @@ RESOURCES_DIR="tile/resources"
 AGENT_VERSION=4.14.0
 CLUSTER_AGENT_VERSION=2.6.0
 NOZZLE_RELEASE_VERSION=86
+BPM_VERSION=1.2.7
 
 # download agent bosh release tarball
 curl -L "https://cloudfoundry.datadoghq.com/datadog-agent/datadog-agent-boshrelease-$AGENT_VERSION.tgz" -o $RESOURCES_DIR/datadog-agent-boshrelease.tgz
@@ -16,3 +17,6 @@ curl -L "https://cloudfoundry.datadoghq.com/datadog-cluster-agent/datadog-cluste
 
 # download nozzle bosh release tarball
 curl -L "https://cloudfoundry.datadoghq.com/datadog-firehose-nozzle/datadog-firehose-nozzle-release-$NOZZLE_RELEASE_VERSION.tgz" -o $RESOURCES_DIR/datadog-firehose-nozzle-release.tgz
+
+# download bpm bosh release tarball
+curl -L "https://bosh.io/d/github.com/cloudfoundry/bpm-release?v=$BPM_VERSION" -o $RESOURCES_DIR/bpm-release.tgz
